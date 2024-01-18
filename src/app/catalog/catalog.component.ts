49d7f1776f96fb6e2614262af674fbba06086885
kcaj21 <alex.kcaj@gmail.com>
2024-01-18 19:22:48 +0000

    first commit

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product.model';
+
+@Component({
+  selector: 'bot-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 1,
+      description:
+        "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
+      name: "Large Cyclops",
+      imageName: "head-big-eye.png",
+      category: "Heads",
+      price: 1220.5,
+      discount: 0.2,
+    };
+  }
+} 
