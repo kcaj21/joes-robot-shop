9999605b6e265fdd6dc9d3ac5298ffc7919c86b3
kcaj21 <alex.kcaj@gmail.com>
2024-01-18 21:10:14 +0000

    ngif done

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -7,7 +7,7 @@ import { IProduct } from './product.model';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
-  products: IProduct[];
+  products: any;
   filter: string = '';
 
 
@@ -190,12 +190,13 @@ export class CatalogComponent {
   }
 
     getImageUrl(product: IProduct) {
+      if (!product) return '';
       return '/assets/images/robot-parts/' + product.imageName;
   }
 
     getFilteredProducts() {
       return this.filter === ''
         ? this.products
-        : this.products.filter((product) => product.category === this.filter);
+        : this.products.filter((product: any) => product.category === this.filter);
     }
 } 
